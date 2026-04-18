e9832fab25789f96f93d85a0c4cde1aed9fa571e
cruzp <cruzp@DESKTOP-O3L7UA1>
2026-04-18 14:14:19 -0300

    Agregar archivos de proyecto.

---
+++ DAL/Acceso.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    internal class Acceso
+    {
+        private SqlConnection conexion;
+        private SqlTransaction tx;
+    
+
+        public void Abrir()
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = "Initial catalog=campo; Data Source=.;integrated security=SSPI";            conexion.Open();
+        }
+
+        public void Cerrar()
+        {
+            conexion.Close();
+            conexion=null;
+            GC.Collect();   
+        }
+
+        public SqlCommand CrearComando(string sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = conexion;
+            return cmd;
+        }
+
+        public int Escribir(String sql)
+        {
+            SqlCommand cmd = CrearComando(sql);
+            int filas = 0;
+
+            try
+            {
+                filas = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                filas = -1;
+            }
+            return filas;
+        }
+        public SqlDataReader Leer(String sql)
+        {
+            SqlCommand cmd = CrearComando(sql);
+            return cmd.ExecuteReader();
+        }
+
+        public int LeerEscalar(String sql)
+        {
+            SqlCommand cmd = CrearComando(sql);
+            return int.Parse(cmd.ExecuteScalar().ToString());
+        }
+
+    }
+}
